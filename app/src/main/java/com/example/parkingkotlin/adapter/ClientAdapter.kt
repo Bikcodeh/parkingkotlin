5f29d4e13feec846d4c84102a6a1c90c43b20544
Bikcode <bikcodeh@gmail.com>
2020-03-03 22:08:49 -0500

    UI: modifica item list client
    -Agrega color verde y amarillo

--- app/src/main/java/com/example/parkingkotlin/adapter/ClientAdapter.kt
+++ app/src/main/java/com/example/parkingkotlin/adapter/ClientAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.parkingkotlin.R
 import com.example.parkingkotlin.database.entity.ClientEntity
@@ -22,11 +23,18 @@ class ClientAdapter(val clientList: List<ClientEntity>): RecyclerView.Adapter<Cl
     override fun onBindViewHolder(holder: ClientViewHolder, position: Int) {
         holder.clientName.text = this.clientList[position].clientName
         holder.clientPlaque.text = this.clientList[position].clientPlaque
+        if(this.clientList[position].clientActive == 1){
+            holder.clientStatus.text = holder.itemView.resources.getString(R.string.up_to_date)
+        }else{
+            holder.clientStatus.text = holder.itemView.resources.getString(R.string.pending)
+            holder.clientStatus.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.green))
+        }
     }
 
     class ClientViewHolder(view: View): RecyclerView.ViewHolder(view){
 
         val clientName: TextView = view.findViewById(R.id.item_list_client_txt_name)
         val clientPlaque: TextView = view.findViewById(R.id.item_list_client_txt_plaque)
+        val clientStatus: TextView = view.findViewById(R.id.item_list_client_status)
     }
 }

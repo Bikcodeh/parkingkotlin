ce91e07171595f50a539074d84b4291e83860334
hoyovi <victor.hoyos@teatedigital.com>
2020-03-06 08:55:57 -0500

    UI: crea splash screen
    -Agrega drawable
    -Agrega strings

---
+++ app/src/main/java/com/example/parkingkotlin/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.parkingkotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.blogspot.atifsoftwares.animatoolib.Animatoo
+import com.example.parkingkotlin.activity.main.view.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT:Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            Animatoo.animateFade(this)
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
